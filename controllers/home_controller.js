6acb4ddbcc38756c94c9b4c654d64abb6a5e9eb0
Zahid Khan <zahidkhanavi@gmail.com>
2024-08-12 22:01:52 +0530

    display post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,24 @@
-module.exports.home = function(req, res){
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
-    return res.render('home', {
-        title: "Home"
-    });
-}
+const Post = require('../models/post');
+
+module.exports.home = async function(req, res) {
+    try {
+        // Fetch all posts
+        let posts = await Post.find({}).populate('user').exec();
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        });
+    } catch (err) {
+        console.log('Error in fetching posts:', err);
+        return res.redirect('back');
+    }
+    // populate the user of each post
+    // Post.find({}).populate('user').exec(function(err, posts){
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts: posts
+    //     });
+    // })
+};
 
 // module.exports.actionName = function(req, res){}
