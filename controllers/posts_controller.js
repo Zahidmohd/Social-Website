2aa43fab79f2da799974999b0a87ac8168f5c131
Zahid Khan <zahidkhanavi@gmail.com>
2024-08-12 20:49:36 +0530

    remove some bug

---
+++ controllers/posts_controller.js
@@ -0,0 +1,14 @@
+const Post = require('../models/post');
+
+module.exports.create = async function(req, res) {
+    try {
+        await Post.create({
+            content: req.body.content,
+            user: req.user._id
+        });
+        return res.redirect('/');
+    } catch (err) {
+        console.log('Error in creating a post:', err);
+        return res.redirect('/');
+    }
+};
