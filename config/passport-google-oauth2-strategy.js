5ad19e1d03f7190d403cf94d55332beeff04774c
Zahid Khan <zahidkhanavi@gmail.com>
2024-08-17 23:33:43 +0530

    added google sign in

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -3,42 +3,39 @@ const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const crypto = require('crypto');
 const User = require('../models/user');
 
-
-// tell passport to use a new strategy for google login
+// Tell passport to use a new strategy for Google login
 passport.use(new googleStrategy({
-        clientID: '<YOUR_GOOGLE_CLIENT_ID>', // e.g. asdfghjkkadhajsghjk.apps.googleusercontent.com
-        clientSecret: '<YOUR_GOOGLE_CLIENT_SECRET>', // e.g. _ASDFA%KFJWIASDFASD#FAD-
-        callbackURL: "http://localhost:8000/users/auth/google/callback",
+        clientID: '953049878561-6n24jb370ng0rlflve3384hsd1ntilk2.apps.googleusercontent.com',
+        clientSecret: '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',
+        callbackURL: "http://localhost:8000/user/auth/google/callback",
     },
 
-    function(accessToken, refreshToken, profile, done){
-        // find a user
-        User.findOne({email: profile.emails[0].value}).exec(function(err, user){
-            if (err){console.log('error in google strategy-passport', err); return;}
+    async function(accessToken, refreshToken, profile, done) {
+        try {
+            // Find a user with the given email
+            let user = await User.findOne({ email: profile.emails[0].value });
+
             console.log(accessToken, refreshToken);
             console.log(profile);
 
-            if (user){
-                // if found, set this user as req.user
+            if (user) {
+                // If found, set this user as req.user
                 return done(null, user);
-            }else{
-                // if not found, create the user and set it as req.user
-                User.create({
+            } else {
+                // If not found, create the user and set it as req.user
+                user = await User.create({
                     name: profile.displayName,
                     email: profile.emails[0].value,
                     password: crypto.randomBytes(20).toString('hex')
-                }, function(err, user){
-                    if (err){console.log('error in creating user google strategy-passport', err); return;}
-
-                    return done(null, user);
                 });
+
+                return done(null, user);
             }
-
-        }); 
+        } catch (err) {
+            console.log('Error in Google strategy-passport:', err);
+            return done(err, false);
+        }
     }
-
-
 ));
-
 
 module.exports = passport;
