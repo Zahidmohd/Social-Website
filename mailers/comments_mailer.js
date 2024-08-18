0d0bc17b346067189d785efcc85b8ab4802ff580
Zahid Khan <zahidkhanavi@gmail.com>
2024-08-18 13:48:15 +0530

    send html template

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -1,15 +1,16 @@
-const nodemailer = require('../config/nodemailer');
+const nodeMailer = require('../config/nodemailer');
 
 
 // this is another way of exporting a method
 exports.newComment = (comment) => {
-    console.log('inside newCommet mailer', comment);
+    let htmlString = nodeMailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs');
+    // console.log('inside newCommet mailer', comment);
 
-    nodemailer.transporter.sendMail({
+    nodeMailer.transporter.sendMail({
         from: 'zahidkhanisme@gmail.com',
         to: comment.user.email,
         subject: "New Comment Published!",
-        html: '<h1>Yup, your comment is now published!</h1>'
+        html: htmlString
         }, (err, info) => {
             if(err) {
                 console.log('Error in sending mail', err);
