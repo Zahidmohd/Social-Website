8b5dd4a101c51714c9db6d594132f2e661c3ac5e
Zahid Khan <zahidkhanavi@gmail.com>
2024-08-18 19:24:17 +0530

    starded and use kue

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,10 +1,13 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 const commentsMailer = require('../mailers/comments_mailer');
+const queue = require('../config/kue');
+const commentEmailWorker = require('../workers/comment_email_worker');
+
 
 module.exports.create = async function(req, res) {
  try {
-        let post = await Post.findById(req.body.post);
+       let post = await Post.findById(req.body.post);
 
        if (!post) {
             req.flash('error', 'Post not found');
@@ -22,7 +25,17 @@ module.exports.create = async function(req, res) {
         await post.save();
 
         comment = await Comment.findById(comment._id).populate('user', 'name email').exec();
-        commentsMailer.newComment(comment);
+        // commentsMailer.newComment(comment);
+        
+        let job = queue.create('emails', comment).save(function(err){
+            if (err){
+                console.log('Error in sending to the queue', err);
+                return;
+            }
+            console.log('job enqueued', job.id);
+
+        })
+
 
         if (req.xhr) {
             // Populate the user field to include only the name of the user
