0e0de8e735f54f3d3789d57681bce2d4de51bed9
Zahid Khan <zahidkhanavi@gmail.com>
2024-08-19 17:34:29 +0530

    added helpers

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const env = require('./config/environment');
+const logger = require('morgan');
+
 const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const passportJWT = require('./config/passport-jwt-strategy');
 const passportGoogle = require('./config/passport-google-oauth2-strategy');
+
 const MongoStore = require('connect-mongo');
 const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
@@ -33,13 +37,16 @@ chatServer.listen(5000, () => {
 });
 const path = require('path');
 
-app.use(sassMiddleware({
-    src: path.join(__dirname, env.asset_path, 'scss'),
-    dest: path.join(__dirname, env.asset_path, 'css'),
-    debug: true,
-    outputStyle: 'extended',
-    prefix: '/css'
-}));
+if (env.name == 'development'){
+    app.use(sassMiddleware({
+        src: path.join(__dirname, env.asset_path, 'scss'),
+        dest: path.join(__dirname, env.asset_path, 'css'),
+        debug: true,
+        outputStyle: 'extended',
+        prefix: '/css'
+    }));
+}
+
 app.use(express.urlencoded());
 
 app.use(cookieParser());
@@ -47,11 +54,14 @@ app.use(express.static('env.asset_path'));
 // make the uploads path available to the browser
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
-app.use(expressLayouts);
+app.use(logger(env.morgan.mode, env.morgan.options));
 
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
+// set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
