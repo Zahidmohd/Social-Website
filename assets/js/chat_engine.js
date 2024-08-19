3ae529476a7b0011c07823d6df61194b2793b445
Zahid Khan <zahidkhanavi@gmail.com>
2024-08-19 13:22:32 +0530

    remobed chatbox bugs

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -1,20 +1,59 @@
-class ChatEngine{
-    constructor(chatBoxId, userEmail){
+class ChatEngine {
+    constructor(chatBoxId, userEmail) {
         this.chatBox = $(`#${chatBoxId}`);
         this.userEmail = userEmail;
 
+        // Establish connection to the server
         this.socket = io.connect('http://localhost:5000');
 
-        if (this.userEmail){
+        if (this.userEmail) {
             this.connectionHandler();
         }
-
     }
 
+    connectionHandler() {
+        let self = this;
 
-    connectionHandler(){
-        this.socket.on('connect', function(){
-            console.log('connection established using sockets...!');
+        // Handle the connection event
+        this.socket.on('connect', function () {
+            console.log('Connection established using sockets!');
+
+            self.socket.emit('join_room', {
+                user_email: self.userEmail,
+                chatroom: 'codeial'
+            });
+
+            self.socket.on('user_joined', function (data) {
+                console.log('A user joined!', data);
+            });
+        });
+
+        // Send a message on clicking the send button
+        $('#send-message').click(function () {
+            let msg = $('#chat-message-input').val();
+
+            if (msg.trim() !== '') {
+                self.socket.emit('send_message', {
+                    message: msg,
+                    user_email: self.userEmail,
+                    chatroom: 'codeial'
+                });
+                $('#chat-message-input').val(''); // Clear the input field after sending
+            }
+        });
+
+        // Listen for incoming messages
+        self.socket.on('receive_message', function (data) {
+            console.log('Message received:', data.message);
+
+            let newMessage = $('<li>').addClass(data.user_email === self.userEmail ? 'self-message' : 'other-message');
+
+            newMessage.append($('<span>').text(data.message));
+            newMessage.append($('<sub>').text(data.user_email));
+
+            $('#chat-messages-list').append(newMessage);
+
+            console.log('Message appended to chatbox.');
         });
     }
 }
--- index.js
+++ index.js
@@ -16,9 +16,20 @@ const customMware = require('./config/middleware');
 
 // setup the chat server to be used with socket.io
 const chatServer = require('http').Server(app);
-const chatSockets = require('./config/chat_socket').chatSockets(chatServer);
-chatServer.listen(5000);
-console.log('chat server is listening oh port 5000');
+const io = require('socket.io')(chatServer, {
+    cors: {
+        origin: "http://localhost:8000",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true
+    }
+});
+
+const chatSockets = require('./config/chat_sockets').chatSockets(io);
+
+chatServer.listen(5000, () => {
+    console.log('Chat server is listening on port 5000');
+});
 
 app.use(sassMiddleware({
     src: './assets/scss',
