3694d3e6d4b4ce3857a7d75b1c76c05d75679122
Zahid Khan <zahidkhanavi@gmail.com>
2024-08-19 19:02:52 +0530

    remove some bug and now it is complete

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,70 +1,67 @@
-const gulp = require('gulp');
+import gulp from 'gulp';
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import cssnano from 'gulp-cssnano';
+import rev from 'gulp-rev';
+import terser from 'gulp-terser';
+import imagemin from 'gulp-imagemin';
+import { deleteAsync } from 'del';
 
-const sass = require('gulp-sass');
-const cssnano = require('gulp-cssnano');
-const rev = require('gulp-rev');
-const uglify = require('gulp-uglify-es').default;
-const imagemin = require('gulp-imagemin');
-const del = require('del');
+const sass = gulpSass(dartSass);
 
-
-
-gulp.task('css', function(done){
+// Define tasks using ES module syntax
+export const css = (done) => {
     console.log('minifying css...');
-    gulp.src('./assets/sass/**/*.scss')
-    .pipe(sass())
-    .pipe(cssnano())
-    .pipe(gulp.dest('./assets.css'));
+    gulp.src('./assets/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(cssnano())
+        .pipe(gulp.dest('./assets/css'));
 
-     gulp.src('./assets/**/*.css')
-    .pipe(rev())
-    .pipe(gulp.dest('./public/assets'))
-    .pipe(rev.manifest({
-        cwd: 'public',
-        merge: true
-    }))
-    .pipe(gulp.dest('./public/assets'));
+    gulp.src('./assets/**/*.css')
+        .pipe(rev())
+        .pipe(gulp.dest('./public/assets'))
+        .pipe(rev.manifest({
+            cwd: 'public',
+            merge: true
+        }))
+        .pipe(gulp.dest('./public/assets'));
     done();
-});
+};
 
-
-gulp.task('js', function(done){
+export const js = (done) => {
     console.log('minifying js...');
-     gulp.src('./assets/**/*.js')
-    .pipe(uglify())
-    .pipe(rev())
-    .pipe(gulp.dest('./public/assets'))
-    .pipe(rev.manifest({
-        cwd: 'public',
-        merge: true
-    }))
-    .pipe(gulp.dest('./public/assets'));
-    done()
-});
+    gulp.src('./assets/**/*.js')
+        .pipe(terser())  // Changed to terser
+        .pipe(rev())
+        .pipe(gulp.dest('./public/assets'))
+        .pipe(rev.manifest({
+            cwd: 'public',
+            merge: true
+        }))
+        .pipe(gulp.dest('./public/assets'));
+    done();
+};
 
-
-gulp.task('images', function(done){
+export const images = (done) => {
     console.log('compressing images...');
     gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)')
-    .pipe(imagemin())
-    .pipe(rev())
-    .pipe(gulp.dest('./public/assets'))
-    .pipe(rev.manifest({
-        cwd: 'public',
-        merge: true
-    }))
-    .pipe(gulp.dest('./public/assets'));
+        .pipe(imagemin())
+        .pipe(rev())
+        .pipe(gulp.dest('./public/assets'))
+        .pipe(rev.manifest({
+            cwd: 'public',
+            merge: true
+        }))
+        .pipe(gulp.dest('./public/assets'));
     done();
-});
+};
 
-
-// empty the public/assets directory
-gulp.task('clean:assets', function(done){
-    del.sync('./public/assets');
+export const cleanAssets = (done) => {
+    deleteAsync(['./public/assets']);
     done();
-});
+};
 
-gulp.task('build', gulp.series('clean:assets', 'css', 'js', 'images'), function(done){
+export const build = gulp.series(cleanAssets, css, js, images, (done) => {
     console.log('Building assets');
     done();
 });
